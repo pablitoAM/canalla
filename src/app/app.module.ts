a54d151b7c36d003f7473c31ea1bbd3155180b6b
PablitoAM <goragalleta@gmail.com>
2017-06-13 23:14:14 +0200

    imported materialize (missing jquery) and some components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MapComponent } from './map/map.component';
-import { LoginComponent } from './login/login.component';
+import { MapComponent } from './components/map/map.component';
+import { LoginComponent } from './components/login/login.component';
+import { TabsComponent } from './components/tabs/tabs.component';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
     AppComponent,    
-    MapComponent, LoginComponent
+    MapComponent, LoginComponent, TabsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'xxxx'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
